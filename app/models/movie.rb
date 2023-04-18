8697cd8fbaeb45bd2b20d1b6ec27ed09e820aacc
Meenoow <100647073+Meenoow@users.noreply.github.com>
2023-04-18 21:03:31 +0000

    Pages working and responding

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,4 +13,22 @@
 #  director_id :integer
 #
 class Movie < ApplicationRecord
+ 
+  def title_with_year
+    return "#{self.title} (#{self.year})"
+  end
+
+  def director
+    d_id = self.director_id
+    matching_directors = Director.where({ :id => d_id })
+    the_director = matching_directors.at(0)
+    return the_director
+  end  
+
+  def characters
+    d_id = self.id
+    matching_characters = Character.where({ :movie_id => d_id })
+    return matching_characters.at(0)
+  end  
+  
 end
